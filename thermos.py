4e8feeb91c4bcd67ea16fc8ba6a978905a3e89b4
chalonhutson <chalonhutson@gmail.com>
2021-09-13 13:45:02 -0600

    Halfway through module 4. Stopped at 'if and for' video and just watched videos from there.

--- thermos.py
+++ thermos.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template, url_for
+from datetime import datetime
+from flask import Flask, render_template, url_for, request, redirect, flash
+
 
 app = Flask(__name__)
+
+bookmarks = []
+app.config["SECRET_KEY"] = b'}C\xe7\xc5!\x7f\x94\xdb\xc8\xe3\xb5\xa7\x82l:^/\x10\x8a\x1f;\xe0\x96\xe5'
+
+
+def store_bookmark(url):
+    bookmarks.append(dict(
+        url = url,
+        user = "Bobberson",
+        date = datetime.utcnow()
+    ))
+
+def new_bookmarks(num):
+    return sorted(bookmarks, key=lambda bm: bm["date"], reverse=True)[:num]
 
 class User:
     def __init__(self, firstname, lastname):
@@ -16,11 +32,16 @@ class User:
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title="Title passed from view to template", user=User("Bob", "Bobberson"))
+    return render_template('index.html', title="Title passed from view to template", user=User("Bob", "Bobberson"), new_bookmarks=new_bookmarks(5))
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    return render_templat("add.html")
+    if request.method == "POST":
+        url = request.form["url"]
+        store_bookmark(url)
+        flash("Stored bookmark '{}'".format(url))
+        return redirect(url_for("index"))
+    return render_template("add.html")
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -33,4 +54,4 @@ def server_error(e):
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
